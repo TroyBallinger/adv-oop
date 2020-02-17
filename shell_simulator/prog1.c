55db6ad8d30e7c6666e6177a3716da50c73049c3
Troy <troy@iconoclasm.re>
2020-02-17 15:29:59 -0500

    shell updates

--- shell_simulator/prog1.c
+++ shell_simulator/prog1.c
@@ -1,178 +1,153 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
+#include <stdbool.h> 
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define CHAR_LIMIT 1024
 #define ARG_LIMIT 64
 
-// Helper function to handle commands 
-void exec_command(char**, unsigned short, unsigned short);
-// Helper function to handle pipelines 
-void exec_pipeline(char**, unsigned short, unsigned short);
+void kill_processes(int);
+void exec_command(char**, short, bool);
+bool running = 0;
 
-// Parses commands that are inputted by the user 
-int main(void) {    
-	// Buffer of user input
+int main(void) {
 	char user_input[CHAR_LIMIT];
-
 	char* args[ARG_LIMIT];
 	unsigned short arg_count;
-	bool exit = 0;
-
-	// Define special chars, give them each a reference address 
-	char pipeline = '|';
-	char end_command = ';';
-	char background = '&';
-	char input = '<';
-	char output = '>';
-
-	// Continue prompting for user input until "exit" is typed 
+	bool exit, background = 0;
+	char special_chars[] = {' ','&','<','>',';','|'};
+	signal(SIGINT, kill_processes);
+	setbuf(stdout, NULL);
 	while (!exit) {
-		// Reset variables 
 		memset(user_input, '\0', CHAR_LIMIT);
 		memset(args, 0, ARG_LIMIT);
 		arg_count = 0;
-
-		// Prompt user 
-		printf("shell_sim> ");
-
-		// Pull user input 
+		background = 0;
+		bool traversing_arg = 0;
+		unsigned short i = 0;
+		fprintf(stdout, "shell> ");
 		fgets(user_input, CHAR_LIMIT, stdin);
-
-		// Counter variable 
-		unsigned short arg_finder = 0; 
-
-		// True while traversing a non-special argument string 
-		bool traversing_arg = 0; 
-
-		// The first char address of every argument is stored in args[]. 
-		while(user_input[arg_finder] != '\n') {
-			// Special chars count as their own arg
-			if (user_input[arg_finder] == '|') {
-				args[arg_count] = &pipeline; 
-			} else if (user_input[arg_finder] == ';') {
-				args[arg_count] = &end_command; 
-			} else if (user_input[arg_finder] == '&') {
-				args[arg_count] = &background; 
-			} else if (user_input[arg_finder] == '<') {
-				args[arg_count] = &input; 
-			} else if (user_input[arg_finder] == '>') {
-				args[arg_count] = &output; 
-			} else if (!traversing_arg && user_input[arg_finder] != ' ') {
-				// Non-special arg found 
-				args[arg_count] = &(user_input[arg_finder]);
+		while (user_input[i] != '\n') {
+			for (unsigned short j = 0; j < 6; j++) {
+				if (user_input[i] == special_chars[j]) {
+					if (j > 1) {
+						args[arg_count] = &(special_chars[j]);
+						arg_count++;					
+					} else if (j == 1) {
+						background = 1;
+					}
+					traversing_arg = 0;
+					user_input[i] = '\0';
+				}
+			}
+			if (!traversing_arg && user_input[i] != '\0') {
+				args[arg_count] = &(user_input[i]);
 				traversing_arg = 1;
 				arg_count++;
 			}
-			// Convert space to a '\0' - denotes end of arg 
-			switch (user_input[arg_finder]) {
-				case '&': 
-				case '|': 
-				case ';': 
-				case '<': 
-				case '>': 
-					arg_count++;
-				case ' ': 
-					traversing_arg = 0;
-					user_input[arg_finder] = '\0';
-				default: break;
-			}
-			arg_finder++;
+			i++;
 		}
+		user_input[i] = '\0';
+		i = 0;
+		unsigned short cmd_start = 0;
+		while (i < arg_count) {
+			if (strcmp(args[i], "exit") == 0) { 
+				exit = 1; 
+				i = arg_count; /* Exit the loop */
+			} else if (args[i][0] == special_chars[4] || i == (arg_count - 1)) {
+				if (i == (arg_count - 1)) { i++; }
+				args[i] = NULL;
+				exec_command(&(args[cmd_start]), (i - cmd_start), background);
+				cmd_start = i + 1;
+			}
+			i++;
+		}
+	}
+	return 0;
+}
 
-		// Set last char (newline) to null terminator 
-		user_input[arg_finder] = '\0';
+void kill_processes(int sig) {
+	if (sig == 2) {
+		kill(0,0);
+		if (!running) {
+			fprintf(stdout, "\nCtrl-C caught. But currently there is no foreground process running.\nshell> ");
+		}
+	}
+}
 
-		// Handle separate commands 
-		unsigned short args_in_command = 0;
-		unsigned short command_start = 0;
-		for (int i = 0; i < arg_count; i++) {
-			if (args[i][0] == ';') {
-				// Handle pipelines 
-				exec_pipeline(args, command_start, args_in_command);
-				printf("\n\n"); // Denote a new command 
-				args_in_command = 0;
-				command_start = i + 1;
-			} else {
-				if (strcmp(args[i], "exit") == 0) {
-					exit = 1;
+void exec_command(char** args, short arg_count, bool background) {
+	unsigned short start_index = 0;
+	int child_pid = fork();
+	if (child_pid == 0) {
+		unsigned short k = 0;
+		unsigned short f = 0;
+		int fd1[2];
+		int fd2[2];
+		pipe(fd1);
+		pipe(fd2);
+		while (k < arg_count) {
+			if (args[k][0] == '<') {
+				int i_fd = open(args[(k+1)], O_RDONLY);
+				args[k] = NULL;
+				args[k + 1] = NULL;
+				dup2(i_fd, 0);
+				k++;
+			} else if (args[k][0] == '>') {
+				int o_fd = open(args[(k+1)], O_CREAT|O_TRUNC|O_RDWR, S_IRWXU);
+				args[k] = NULL;
+				args[k + 1] = NULL;
+				dup2(o_fd, 1);
+				k++;
+			} else if (args[k][0] == '|') {
+				args[k] = NULL;
+				int p_pid = fork();
+				if (p_pid == 0) {
+					switch (f) {
+						case 0: dup2(fd1[1], 1);
+							close(fd1[0]);
+							close(fd1[1]); break;
+						default: dup2(fd2[1], 1);
+							close(fd2[0]);
+							close(fd2[1]); break;
+					}
+					execvp(args[start_index], &(args[start_index]));
 				} else {
-					args_in_command++;
+					waitpid(p_pid, NULL, 0);
+					fprintf(stdout, "\n... child process (PID=%d) is done\n", p_pid);
+					dup2(fd1[0], 0);
+					switch (f) {
+						case 0: dup2(fd1[0], 0);
+							close(fd1[0]);
+							close(fd1[1]); break;
+						default: dup2(fd2[0], 0);
+							close(fd2[0]);
+							close(fd2[1]); break;
+					}
+					start_index = k + 1;
+					f++;
 				}
 			}
-		}
-		exec_pipeline(args, command_start, args_in_command);
-	}
-}
-
-void exec_command(char** args, unsigned short arg_start, unsigned short arg_count) {
-	if (arg_count == 0) { return; }
-	// Print the command 
-	printf("COMMAND: %s", args[arg_start]);
-	if (arg_count > 1) { printf(", "); }
-	// Print arguments after 
-	unsigned short null_args = 0;
-	for (int i = 1; i < arg_count; i++) {
-		if (args[i] != 0) {
-			printf("arg-%d: %s", (i - null_args), args[arg_start + i]);
-			if (i < (arg_count - 1)) { printf(", "); }
-		} else {
-			// Adjust for removed arguments
-			null_args++;
-			arg_count++;
-		}
-	}
-	printf("\n");
-}
-
-void exec_pipeline(char** args, unsigned short command_start, unsigned short args_in_command) {
-	unsigned short args_in_pipe = 0;
-	unsigned short pipe_start = command_start;
-	bool background = 0;
-	char* input = 0;
-	char* output = 0;
-	for (int j = command_start; j < command_start + args_in_command; j++) {
-		if (args[j][0] == '|') {
-			exec_command(args, pipe_start, args_in_pipe);
-			if (input != 0) {
-				printf("... input of the above command will be redirected from file \"%s\".\n", input);
+			if (k == (arg_count - 1)) {
+				execvp(args[start_index], &(args[start_index]));
 			}
-			if (output != 0) {
-				printf("... output of the above command will be redirected to file \"%s\".\n", output);
-			}
-			printf("... output of the above command will be redrecited to serve as the input of the following command ...\n");
-			input = 0;
-			output = 0;
-			args_in_pipe = 0;
-			pipe_start = j + 1;
-		} else if (args[j][0] == '&') { 
-			// Set to background execution 
-			background = 1;
-		} else if (args[j][0] == '<') {
-			// Remove input argument 
-			input = args[j + 1];
-			args[j] = 0;
-			args[j + 1] = 0; 
-			j++;
-		} else if (args[j][0] == '>') {
-			// Remove output argument 
-			output = args[j + 1];
-			args[j] = 0;
-			args[j + 1] = 0; 
-			j++;
-		} else {
-			args_in_pipe++;
+			k++;
 		}
-	}
-	// Execute last pipe 
-	exec_command(args, pipe_start, args_in_pipe); 
-	if (input != 0) {
-		printf("... input of the above command will be redirected from file \"%s\".\n", input);
-	}
-	if (output != 0) {
-		printf("... output of the above command will be redirected to file \"%s\".\n", output);
-	}
-	if (background) {
-		printf("... the above command will be executed in background.\n");
+	} else {
+		if (background) {
+			waitpid(child_pid, NULL, WNOHANG);
+		} else {
+			running = 1;
+			waitpid(child_pid, NULL, 0);
+			running = 0;
+			fprintf(stdout, "\n... child process (PID=%d) is done\n", child_pid);
+			int pid;
+			while ((pid = waitpid(-1, NULL, WNOHANG)) > 0) {
+				fprintf(stdout, "\n... child process (PID=%d) is done\n", pid);
+			}
+		}
 	}
 }
